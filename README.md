5ab536a53c9a396b30cd06321535a715f65e2210
jamesp9 <james@jp-it.net.au>
2016-10-16 21:35:09 +1100

    ds18b20: use same plugin for multiple devices

--- README.md
+++ README.md
@@ -4,6 +4,32 @@ Graph assorted sensors attached to the Raspberry Pi
 ## Internal Temperature Sensor
 
 ## DS18B20 - Temperature
+To use the Dallas 1-Wire Temperature sensor plugin, you will first need to find out the unique 64bit rom code of your device.
+```
+ls -l /sys/bus/w1/devices/
+```
+You will see something like this:
+```
+lrwxrwxrwx 1 root root 0 Oct 10 08:31 28-000008299930 -> ../../../devices/w1_bus_master1/28-000008299930
+lrwxrwxrwx 1 root root 0 Oct 10 10:37 w1_bus_master1 -> ../../../devices/w1_bus_master1
+```
+For this plugin we only need the unique code `000008299930` as the family id `28` is presumed.
 
+Copy the `sensor_temp_ds18b20.py` script to your system plugins directory:
+On Archlinux:
+```
+# cp sensor_temp_ds18b20.py /usr/lib/munin/plugins/sensor_temp_ds18b20_
+```
+On Debian family:
+```
+# cp sensor_temp_ds18b20.py /usr/share/munin/plugins/sensor_temp_ds18b20_
+```
+
+Now link the plugin using the unique code from your device
+```
+# ln -s /usr/lib/munin/plugins/sensor_temp_ds18b20_ /etc/munin/plugins/sensor_temp_ds18b20_000008299930
+```
+
+Restart the Munin Node service
 
 ## RHT03 - Temperature and Humidity
--- sensor_temp_ds18b20.py
+++ sensor_temp_ds18b20.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 
+# Each Dallas 1-Wire has a unique 64bit it stored in ROM
+# 18b20 has family code 28h
+
+# This plugin can be used for multiple temperature sensors attached to the bus.
+
 import sys
 import os
 
-def run_plugin():
-    w1_id = '28-0000053132a2'
+
+def get_id():
+    plugin_link = os.path.basename(os.path.abspath(__file__))
+    script_name = 'sensor_temp_ds18b20_'
+    return '28-' + plugin_link.replace(script_name, '')
+
+
+def run_plugin(w1_id):
     w1_slave = '/sys/bus/w1/devices/{}/w1_slave'.format(w1_id)
 
     if not os.path.exists(w1_slave):
@@ -27,13 +38,14 @@ def run_plugin():
 
 if __name__ == '__main__':
     num_args = len(sys.argv)
+    rom_id = get_id()
 
     if num_args == 1:
-        print('temperature.value {}'.format(run_plugin()))
+        print('temperature.value {}'.format(run_plugin(rom_id)))
     elif num_args == 2 and sys.argv[1] == 'config':
         print('graph_args --base 1000')
         print('graph_category sensors')
-        print('graph_info Raspberry Pi DS18B20 1-Wire Bus Temperature Sensor')
-        print('graph_title DS18B20 Temperature')
+        print('graph_info DS18B20 1-Wire Bus Temperature Sensor')
+        print('graph_title Temperature DS18B20 Device: {}'.format(rom_id))
         print('graph_vlabel Celsius')
         print('temperature.label ds18b20')
