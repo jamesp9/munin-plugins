754dbaf2f47ab1a5c1069a761873ae89ff09e341
james pinkster <james@jp-it.net.au>
2017-09-05 09:43:40 +1000

    update internal temp sensor to handle more than 1 thermal_zone

--- sensor_temp_internal.py
+++ sensor_temp_internal.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python
+"""
+"""
 
-import sys
-import os
+from os import listdir, path
+from sys import argv, exit
+
+def get_thermals():
+    """
+    """
+    thermals = listdir('/sys/devices/virtual/thermal')
+    thermal_zones = [x for x in thermals if 'thermal_zone' in x]
+    if len(thermal_zones) == 0:
+        print('No thermal_zone devices.')
+        exit()
+    thermal_zones.sort()
+    tz_nums = [x.replace('thermal_zone', '') for x in thermal_zones]
+    return tz_nums
 
 
-def run_plugin():
-    sys_temp = '/sys/devices/virtual/thermal/thermal_zone0/temp'
 
-    if not os.path.exists(sys_temp):
+def run_plugin(tz_num):
+    sys_temp = '/sys/devices/virtual/thermal/thermal_zone{}/temp'.format(tz_num)
+
+    if not path.exists(sys_temp):
         print('Cannot open {}'.format(sys_temp))
-        sys.exit()
+        exit()
 
     with open(sys_temp, 'r') as fo:
         temperature = fo.read()
@@ -18,15 +33,19 @@ def run_plugin():
     temperature = int(temperature) / 1000
     return temperature
 
+
 if __name__ == '__main__':
-    num_args = len(sys.argv)
+    num_args = len(argv)
+    tz_nums = get_thermals()
 
     if num_args == 1:
-        print('temperature.value {}'.format(run_plugin()))
-    elif num_args == 2 and sys.argv[1] == 'config':
+        for tz in tz_nums:
+            print('temperature{}.value {}'.format(tz, run_plugin(tz)))
+    elif num_args == 2 and argv[1] == 'config':
         print('graph_args --base 1000')
         print('graph_category sensors')
         print('graph_info In-Built Temperature Sensor')
         print('graph_title Internal Temperature')
         print('graph_vlabel Celsius')
-        print('temperature.label internal')
+        for tz in tz_nums:
+            print('temperature{0}.label internal {0}'.format(tz))
